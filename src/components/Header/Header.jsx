8e0ec1e7b0371e10faee6cf378fcd77b6fd03ad9
ajek1993@gmail.com <ajek1993@gmail.com>
2024-01-25 14:10:22 +0100

    add header with logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,10 @@
 import React from "react";
+import Menu from "./Menu";
 
 export default function Header() {
   return (
     <>
-      <header className="header_desktop w-full hidden md:block pt-6 pb-2">
-        <nav className="max-w-7xl mx-auto flex justify-between items-center">
-          <div className="logo">Logo</div>
-          <ul className="main__navigation flex gap-x-4">
-            <li className="main__navigation__button">Home</li>
-            <li className="main__navigation__button">About Me</li>
-            <li className="main__navigation__button">Projects</li>
-            <li className="main__navigation__button">Contact</li>
-          </ul>
-        </nav>
-      </header>
-      <header className="header_mobile w-full block md:hidden pt-6 pb-2">
-        <nav className="max-w-7xl mx-auto flex justify-between items-center">
-          <div className="logo">Logo</div>
-          <ul className="main__navigation flex flex-col gap-x-4">
-            <li className="main__navigation__button">Home</li>
-            <li className="main__navigation__button">About Me</li>
-            <li className="main__navigation__button">Projects</li>
-            <li className="main__navigation__button">Contact</li>
-          </ul>
-        </nav>
-      </header>
+      <Menu />
     </>
   );
 }
---
+++ src/components/Header/Menu.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarItem,
+  Link,
+} from "@nextui-org/react";
+
+export default function Menu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = ["Home", "About me", "Projects", "Contact"];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth="xl" className="pt-4">
+      <NavbarContent>
+        <NavbarBrand>
+          <Image src="/rocket-solid.svg" width={40} height={40} alt="Logo" />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {menuItems.map((item, index) => (
+          <NavbarItem key={`${item}-${index}`}>
+            <Link
+              className="hover:text-primary text-xl"
+              color="foreground"
+              href="#"
+            >
+              {item}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarMenu className="mt-4">
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="hover:text-primary w-full"
+              color="foreground"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        className="sm:hidden"
+      />
+    </Navbar>
+  );
+}
