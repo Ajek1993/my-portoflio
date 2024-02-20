67edfbd680c2dcc3660d3347b74f4b62d16a3e3e
Arkadiusz Sarach <ajek1993@gmail.com>
2024-02-20 15:50:50 +0100

    footer animation delete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,6 @@
 export default function Footer() {
   return (
-    <footer
-      data-aos="fade-left"
-      data-aos-duration="2000"
-      className="font-orbitron py-12 md:py-20 mx-auto text-center text-lg md:text-xl overflow-hidden"
-    >
+    <footer className="py-12 md:py-28 mx-auto text-center text-lg md:text-xl overflow-hidden">
       <span className="text-primary">&copy;</span> 2024 Created by Arkadiusz
       Sarach
     </footer>
