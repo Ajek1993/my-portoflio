74ec312b7bfe6cc3bdfc9e29c1fa8a23b10d2ebf
Ajek1993 <ajek1993@gmail.com>
2026-02-06 13:58:47 +0100

    Zmiana marginesów nagłówków sekcji: mt 2.5rem, mb 4.5rem

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -4,7 +4,7 @@ import "./Heading.css";
 export default function Heading({ title }) {
   return (
     <h2
-      className="section__header text-5xl font-bold text-center my-14 relative bg-gradient-to-r from-primary to-primary-light bg-clip-text text-transparent"
+      className="section__header text-5xl font-bold text-center mt-10 mb-[4.5rem] relative bg-gradient-to-r from-primary to-primary-light bg-clip-text text-transparent leading-tight"
       data-aos="fade-up"
     >
       {title}
